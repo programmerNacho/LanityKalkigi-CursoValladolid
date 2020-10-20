d99c92ef55bd131fa8bfd31d8db96861a181294c
RIV3E <RIV3E@github.com>
2020-10-20 11:09:21 +0200

    Update mob.cs

--- Assets/Scripts/Enemy/mob.cs
+++ Assets/Scripts/Enemy/mob.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mob : MonoBehaviour
+public class Mob : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] spawnPoints;
