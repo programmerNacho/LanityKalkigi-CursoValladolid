03a294ea7bca1c76bef490282bb10fb91a1677fe
RIV3E <RIV3E@github.com>
2020-10-20 12:15:13 +0200

    textures

---
+++ Assets/Scripts/DrawingBlock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawingBlock : MonoBehaviour
+{
+    [SerializeField]
+    private List<Texture2D> smallDrawings;
+    [SerializeField]
+    private List<Texture2D> mediumDrawings;
+    [SerializeField]
+    private List<Texture2D> largeDrawings;
+
+    public Texture2D GiveMeADrawing(PlayerPaint.TextureSizes size)
+    {
+        Texture2D texture = null;
+
+        switch (size)
+        {
+            case PlayerPaint.TextureSizes.Small:
+                texture = smallDrawings[Random.Range(0, smallDrawings.Count)];
+                break;
+            case PlayerPaint.TextureSizes.Medium:
+                texture = mediumDrawings[Random.Range(0, smallDrawings.Count)];
+                break;
+            case PlayerPaint.TextureSizes.Large:
+                texture = largeDrawings[Random.Range(0, smallDrawings.Count)];
+                break;
+        }
+
+        return texture;
+    }
+}
