7fbc3913969f18fcf940eb2d5aa0f384f4ffa7f3
natchcomics <ignaciobarrientos23@gmail.com>
2020-10-21 14:28:51 +0200

    canvas 2

--- Assets/Scripts/Canvas/LevelScript.cs
+++ Assets/Scripts/Canvas/LevelScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,6 +10,7 @@ public class LevelScript : MonoBehaviour
     // Acceder al componente texto para que cada vez que se termine un nivel salte al siguiente número;
     // Si se resetea la escena que el número vuelva 1 otra vez; 
 
+    [SerializeField]
     public Text levelText;
 
     [SerializeField]
@@ -20,8 +22,16 @@ public class LevelScript : MonoBehaviour
     public void NewScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-
     }
+
+    //public void ChangeLevelText()
+    //{
+    //    // Si termino el dibujo sumo;
+    //    // Si acabo la tinta y tengo Game Over reseteo a 1;
+
+    //    levelText.text = 
+
+    //}
 
     public void ChangeDrawing()
     {
